d9f991a597904fd41fb524bff0afea8937119cd7
alex-s-nik <to_ctrl@mail.ru>
2023-01-30 21:54:56 +0300

    feat: add admin site

--- backend/mylibrary/library/admin.py
+++ backend/mylibrary/library/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, History, Visitor
+
+
+class BaseAdmin(admin.ModelAdmin):
+    fields = ('created_by', 'created_at',)
+    readonly_fields = ('created_at',)
+
+
+class BookAdmin(BaseAdmin):
+    fields = ('title', 'description',) + BaseAdmin.fields
+    list_display = ('title', 'description', 'in_library')
+
+    
+class VisitorAdmin(BaseAdmin):
+    fields = ('name', 'info',) + BaseAdmin.fields
+
+
+class HistoryAdmin(BaseAdmin):
+    fields = ('visitor', 'book', 'action_type',) + BaseAdmin.fields
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Visitor, VisitorAdmin)
+admin.site.register(History, HistoryAdmin)
