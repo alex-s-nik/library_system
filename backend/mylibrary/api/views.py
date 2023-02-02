4b7c3ce98267cb5ee92db093f8453dc20ef2115f
alex-s-nik <to_ctrl@mail.ru>
2023-02-02 16:27:19 +0300

    refactor: add CreatedByMixin

--- backend/mylibrary/api/views.py
+++ backend/mylibrary/api/views.py
@@ -4,18 +4,17 @@ from rest_framework.permissions import IsAuthenticated
 from api.serializers import BookSerializer, VisitorSerializer
 from library.models import Book, Visitor
 
-class BookViewSet(viewsets.ModelViewSet):
+
+class CreatedByMixin:
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class BookViewSet(CreatedByMixin, viewsets.ModelViewSet):
     queryset = Book.objects.all()
     permission_classes = (IsAuthenticated,) # need to chenge to librarian and admin
     serializer_class = BookSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
-
-class VisitorViewSet(viewsets.ModelViewSet):
+class VisitorViewSet(CreatedByMixin, viewsets.ModelViewSet):
     queryset = Visitor.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = VisitorSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user)
