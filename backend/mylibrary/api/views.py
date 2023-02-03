fd92c7d1b8e8ca4b64f479c2b00dd25e9cfca030
alex-s-nik <to_ctrl@mail.ru>
2023-02-03 16:19:00 +0300

    in progress: extra actions

--- backend/mylibrary/api/views.py
+++ backend/mylibrary/api/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api.permissions import IsLibrarianAndAbove
 from api.serializers import BookSerializer, VisitorSerializer
-from library.models import Book, Visitor
+from library.models import ActionType, Book, History, Visitor
 
 
 class CreatedByMixin:
@@ -15,7 +18,43 @@ class BookViewSet(CreatedByMixin, viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsLibrarianAndAbove)
     serializer_class = BookSerializer
 
+    @action(
+        detail = True,
+        methods = ['post'],
+        permission_classes = (IsAuthenticated,),
+        url_path = 'taken_by/(?P<visitor_id>[^/.]+)'
+    )
+    def taken_by(self, request, visitor_id, id=None):
+        book = get_object_or_404(Book, id=id)
+        visitor = get_object_or_404(Visitor, id=visitor_id)
+        # !!check if the book is already taken
+        history = History.objects.create(
+            visitor = visitor,
+            book = book,
+            action_type = ActionType.TAKE,
+            created_by = request.user
+        )
+        history.save()
+        return Response(..., status=status.HTTP_201_CREATED)
+    
+    @action(
+        detail=True,
+        methods=['post'],
+        permission_classes = (IsAuthenticated,)
+    )
+    def was_returned(self, request, id=None):
+        ...
+
+
 class VisitorViewSet(CreatedByMixin, viewsets.ModelViewSet):
     queryset = Visitor.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = VisitorSerializer
+
+    @action(
+        detail=True,
+        methods=['post'],
+        permission_classes=(IsAuthenticated,)
+    )
+    def books(self, request, id=None):
+        ...
