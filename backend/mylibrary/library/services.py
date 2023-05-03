c70a396155787b104d70fc3f22ff029f5fdc2ba1
alex-s-nik <to_ctrl@mail.ru>
2023-05-03 15:34:03 +0300

    refactor: actions

--- backend/mylibrary/library/services.py
+++ backend/mylibrary/library/services.py
@@ -1,27 +1,33 @@
 from datetime import datetime
 
+from django.db import IntegrityError
+
 from .models import Action, Book, Visitor
 from users.models import User
 
 
 def take_book_to_visitor(book: Book, visitor: Visitor, executor: User):
     if not book.in_library:
-        raise Exception('The book has been taken for other visitor')
+        raise IntegrityError('The book has been taken for other visitor')
+    book.in_library = False
     Action.objects.create(
         book=book,
         visitor=visitor,
         taken_by=executor
     )
+    book.save()
 
 
 def return_book_to_library(book: Book, executor: User):
     if book.in_library:
-        raise Exception('The book is in library already')
+        raise IntegrityError('The book is in library already')
     taken_action = Action.objects.get(
         book=book,
         return_date=None
     )
     taken_action.return_date = datetime.now()
     taken_action.returned_by = executor
-    print(taken_action)
     taken_action.save()
+
+    book.in_library = True
+    book.save()
--- backend/tests/test_actions.py
+++ backend/tests/test_actions.py
@@ -1,25 +1,46 @@
+from django.db import IntegrityError
 import pytest
 
-from library.models import Action
+from library.models import Action, Book
 from library.services import return_book_to_library, take_book_to_visitor
 
 class TestActions:
     @pytest.mark.django_db(transaction=True)
-    def test_take_book_to_visitor(self, book1, visitor1, first_user):
+    def test_book_moving(self, book1, visitor1, first_user):
+        """Тест на выдачу  и прием книги обратно."""
+
+        assert Book.objects.count() > 0, 'В БД нет книг'
+        book = Book.objects.get(id=book1.id)
+
         take_book_to_visitor(
-            book=book1,
+            book=book,
             visitor=visitor1,
             executor=first_user
         )
 
-        assert book1.in_library == False
+        assert book.in_library == False, 'У выданной книги статус должен быть "не в библиотеке"'
 
+        # попытка выдать выданную книгу
+        with pytest.raises(IntegrityError) as msg:
+            take_book_to_visitor(
+                book=book,
+                visitor=visitor1,
+                executor=first_user
+            )
 
         return_book_to_library(
-            book=book1,
+            book=book,
             executor=first_user
         )
 
-        assert Action.objects.count() == 1
-        assert book1.in_library == True
+        #попытка сдать сданную книгу
+        with pytest.raises(IntegrityError) as msg:
+            return_book_to_library(
+                book=book,
+                executor=first_user
+            )
 
+        assert Action.objects.count() == 1
+        action = Action.objects.all()[0]
+        assert action.return_date is not None
+        assert book.in_library == True, 'У книги возвращенной в библиотеку статус должен быть "в библиотеке"'
