7d75bdf03cf3aea89dff1b72ba7d48894c718700
alex-s-nik <to_ctrl@mail.ru>
2023-05-04 16:37:59 +0300

    feat: add bootstrap for api tests

---
+++ backend/tests/test_api.py
@@ -0,0 +1,16 @@
+import pytest
+
+class TestAPIMainModels:
+    def test_visitor(self):
+        pass
+
+    def test_book(self):
+        pass
+
+
+class TestAPIActions:
+    def test_taking_book(self):
+        pass
+
+    def test_returning_book(self):
+        pass
