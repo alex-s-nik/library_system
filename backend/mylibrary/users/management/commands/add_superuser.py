4bca29e178fac83b75831ee95be232f0c16c3bcf
alex-s-nik <to_ctrl@mail.ru>
2023-06-28 14:19:58 +0300

    feat: add migrate and create su commands

---
+++ backend/mylibrary/users/management/commands/add_superuser.py
@@ -0,0 +1,25 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+
+
+User = get_user_model()
+
+username = os.getenv('ADMIN_USER', 'root')
+password = os.getenv('ADMIN_PASSWORD', 'root')
+
+
+class Command(BaseCommand):
+    help = "Adds a superuser into database."
+
+    def handle(self, *args, **options):
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                password=password,
+                email=''
+            )
+        else:
+            raise CommandError('User "%s" is already exists' % username)
