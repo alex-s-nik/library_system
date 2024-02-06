b6b1fd3edda45a6490a51be5f559b4084027eed7
Alex Streltsov <to_ctrl@mail.ru>
2024-02-06 14:56:15 +0300

    feat: initialization reader_lending page with reader info

--- front_demo/demo.js
+++ front_demo/demo.js
@@ -1,6 +1,7 @@
 readers = library_data["readers"]
+books = library_data["books"]
 
-
+/* ======== для find_reader.html =========*/
 findReaderInput = document.getElementById("find-reader_input")
 resultsBlock = document.getElementById("find_reader_result")
 
@@ -55,7 +56,7 @@ function findReaderByNameAndCard(event) {
             
 
             //resultHTMLToPrint += "<p class=\"find_results__reader\">" + "<a href=\"#\">" + reader_name + " " + reader_card + "</a>" + "</p>"
-            resultHTMLToPrint += "<p class=\"find_results__reader\">" + "<a class=\"btn btn-light\" href=\"#\" role=\"button\">" + reader_name + " " + reader_card + "</a>" + "</p>"
+            resultHTMLToPrint += "<p class=\"find_results__reader\">" + "<a class=\"btn btn-light\" href=\"reader_lending.html?reader_id=" + reader["id"] +"\" role=\"button\">" + reader_name + " " + reader_card + "</a>" + "</p>"
         }
     });
     if (count > maxRecordsForOutput) {
@@ -63,5 +64,53 @@ function findReaderByNameAndCard(event) {
     }
     resultsBlock.innerHTML = resultHTMLToPrint
 }
+try {
+    findReaderInput.addEventListener("input", findReaderByNameAndCard)
+} catch {
+    console.log("....")
+}
 
-findReaderInput.addEventListener("input", findReaderByNameAndCard)
+
+/* ==end=of== для find_reader.html ========== */
+
+/* ========== для reader_lending.html ========== */
+
+const reader_id_for_page = new URLSearchParams(window.location.search).get("reader_id")
+const reader_for_page = readers.find((reader) => reader["id"] == reader_id_for_page)
+
+const reader_name = reader_for_page["name"]
+const reader_info = "Читательский билет № " + reader_for_page["card"]
+
+const reader_name_element = document.getElementById("reader_lending__reader-info__name")
+reader_name_element.innerText = reader_name
+
+const reader_info_element = document.getElementById("reader_lending__reader-info__info")
+reader_info_element.innerText = reader_info
+
+/* заполнение таблицы выданных книг */
+const reader_lending_facts_element = document.getElementById("reader_lending__reader-info__books_info_table")
+
+const reader_lending_facts = reader_for_page["lending_facts"]
+reader_lending_facts.forEach((fact) => {
+    current_book_id = fact["book_id"]
+    current_book = books.find((book) => book["id"] == current_book_id)
+
+    current_book_info = current_book["title"] + " " + current_book["author"] + " " + current_book["pages"] + " с. " + current_book["year"]
+    current_book_taken_date = fact["taken_date"]
+    current_book_returned_date = fact["returned_date"] === null ? "" : fact["returned_date"]
+
+    td_title = document.createElement('td')
+    td_title.innerText = current_book_info
+    td_taken_date = document.createElement('td')
+    td_taken_date.innerText = current_book_taken_date
+    td_returned_date = document.createElement('td')
+    td_returned_date.innerText = current_book_returned_date
+
+    reader_info_element_row = reader_lending_facts_element.insertRow()
+    reader_info_element_row.appendChild(td_title)
+    reader_info_element_row.appendChild(td_taken_date)
+    reader_info_element_row.appendChild(td_returned_date)
+})
+
+/* ==end=of== для reader_lending.html ========== */
+
