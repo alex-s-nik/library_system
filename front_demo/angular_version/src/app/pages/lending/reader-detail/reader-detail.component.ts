fbc19ee36f344c6e6059929cd01ba33ee6c410ac
alex-s-nik <to_ctrl@mail.ru>
2024-02-13 22:32:52 +0300

    feat: add reader detail component

---
+++ front_demo/angular_version/src/app/pages/lending/reader-detail/reader-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reader-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './reader-detail.component.html',
+  styleUrl: './reader-detail.component.less'
+})
+export class ReaderDetailComponent {
+
+}
